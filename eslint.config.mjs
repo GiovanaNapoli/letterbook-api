a21194bed17b6276281c224c7f67096d7967167f
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-07-09 19:41:37 -0300

    feat: add husky rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts}"]
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: {
+      prettier: prettier,
+    },
   },
   {
     languageOptions: { globals: globals.browser },
   },
   {
     rules: {
-        "no-unused-vars": "error",
-        "no-undef": "error"
-    }
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'prettier/prettier': 2,
+    },
   },
+  prettierRecommended,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-import {z} from 'zod';
+import { z } from 'zod';
 import 'dotenv/config';
 
 const schema = z.object({
